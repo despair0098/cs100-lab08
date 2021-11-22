19212a8e5dce56a1be8b1dda2ac19768aaf4906e
despair0098 <zijunwang324@gmail.com>
2021-11-21 16:09:40 -0800

    Implemented all virtual functions from Base

--- Div.hpp
+++ Div.hpp
@@ -29,6 +29,25 @@ class Div: public Base{
 			}
 			return "(" + left->stringify() + "/" +  right->stringify() + ")";
 		}
+		virtual int number_of_children(){return 2;}
+        virtual Base* get_child(int i){
+            if(i == 0){
+				return left;
+			} else if (i == 1){
+				return right;
+			} else {
+				return nullptr;
+			}
+        }
+        virtual void accept(Visitor* visitor, int index){
+            if(index == 0){
+				visitor->visit_div_begin(this);
+			} else if(index == 1){
+				visitor->visit_div_middle(this);
+			} else if(index == 2){
+				visitor->visit_div_end(this);
+			}
+        }
 };
 
 #endif
--- Pow.hpp
+++ Pow.hpp
@@ -17,6 +17,25 @@ class Pow: public Base{
 		Pow(Base* a, Base* b){value = pow(a->evaluate(),b->evaluate()); this->left = a; this->right = b;}
 		virtual double evaluate() {return value;}
 		virtual string stringify() {return "(" + left->stringify() + "**" +  right->stringify() + ")";}
+		virtual int number_of_children(){return 2;}
+        virtual Base* get_child(int i){
+            if(i == 0){
+				return left;
+			} else if (i == 1){
+				return right;
+			} else {
+				return nullptr;
+			}
+        }
+        virtual void accept(Visitor* visitor, int index){
+            if(index == 0){
+				visitor->visit_pow_begin(this);
+			} else if(index == 1){
+				visitor->visit_pow_middle(this);
+			} else if(index == 2){
+				visitor->visit_pow_end(this);
+			}
+        }
 };
 
 #endif
