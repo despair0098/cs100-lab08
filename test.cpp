8ce52a0783024de08583fd0ddd77c2541a0190d1
YanaWanas <garrickchiu0723@gmail.com>
2021-11-22 23:57:40 -0800

    Finished All Test Cases

--- test.cpp
+++ test.cpp
@@ -13,7 +13,22 @@
 #include "visitor.hpp"
 
 using namespace std;
+
 TEST(Iterator, Add) {
+    Base* three = new Op(3);
+    Base* seven = new Op(7);
+    Base* four = new Op(4);
+    Base* two = new Op(2);
+    Base* add = new Add(three, two);
+
+    concreteVisitor* v = new concreteVisitor();
+    string latex = v->PrintLateX(add);
+    EXPECT_EQ("(3.000000 + 2.000000)", add->stringify());
+    EXPECT_EQ(5, add->evaluate());
+    EXPECT_EQ("${({3.000000}+{2.000000})}$", latex);
+}
+
+TEST(Iterator, Mult) {
     Base* three = new Op(3);
     Base* seven = new Op(7);
     Base* four = new Op(4);
@@ -27,6 +42,64 @@ TEST(Iterator, Add) {
     EXPECT_EQ("(7.000000 * 4.000000)", mult->stringify());
     EXPECT_EQ(28, mult->evaluate());
     EXPECT_EQ("${({7.000000}\\cdot{4.000000})}$", latex);
+}
+
+TEST(Iterator, Sub) {
+    Base* three = new Op(3);
+    Base* seven = new Op(7);
+    Base* four = new Op(4);
+    Base* two = new Op(2);
+    Base* sub = new Sub(seven, four);
+
+    concreteVisitor* v = new concreteVisitor();
+    string latex = v->PrintLateX(sub);
+    EXPECT_EQ("(7.000000 - 4.000000)", sub->stringify());
+    EXPECT_EQ(3, sub->evaluate());
+    EXPECT_EQ("${({7.000000}-{4.000000})}$", latex);
+}
+
+TEST(Iterator, Div) {
+    Base* three = new Op(3);
+    Base* seven = new Op(7);
+    Base* four = new Op(4);
+    Base* two = new Op(2);
+    Base* div = new Div(four, two);
+
+    concreteVisitor* v = new concreteVisitor();
+    string latex = v->PrintLateX(div);
+    EXPECT_EQ("(4.000000 / 2.000000)", div->stringify());
+    EXPECT_EQ(2, div->evaluate());
+    EXPECT_EQ("${\\frac{4.000000}{2.000000}}$", latex);
+}
+
+TEST(Iterator, Pow) {
+    Base* three = new Op(3);
+    Base* seven = new Op(7);
+    Base* four = new Op(4);
+    Base* two = new Op(2);
+    Base* pow = new Pow(seven, four);
+
+    concreteVisitor* v = new concreteVisitor();
+    string latex = v->PrintLateX(pow);
+    EXPECT_EQ("(7.000000 ** 4.000000)", pow->stringify());
+    EXPECT_EQ(2401, pow->evaluate());
+    EXPECT_EQ("${({7.000000}^{4.000000})}$", latex);
+}
+
+TEST(Iterator, CombinedFunctions) {
+    Base* three = new Op(3);
+    Base* seven = new Op(7);
+    Base* four = new Op(4);
+    Base* two = new Op(2);
+    Base* mult = new Mult(seven, four);
+    Base* add = new Add(three, mult);
+    Base* minus = new Sub(add, two);
+
+    concreteVisitor* v = new concreteVisitor();
+    string latex = v->PrintLateX(minus);
+    EXPECT_EQ("((3.000000 + (7.000000 * 4.000000)) - 2.000000)", minus->stringify());
+    EXPECT_EQ(29, minus->evaluate());
+    EXPECT_EQ("${({({3.000000}+{({7.000000}\\cdot{4.000000})})}-{2.000000})}$", latex);
 }
 
 TEST(CombinedTests, Combined) {
